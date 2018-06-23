f08d4b117cea8727286179ab0388db09a0d67ef8
Daniel Laycock <b7029510@my.shu.ac.uk>
2018-06-23 19:16:37 +0100

    Finished App

---
+++ DavidLuiz/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DavidLuiz
+{
+    public partial class Form1 : Form
+    {
+      
+        public Form1()
+        {
+            InitializeComponent();
+            playSimpleSound();
+        }
+
+        private void playSimpleSound()
+        {
+            Stream str = Properties.Resources.tune;
+            SoundPlayer snd = new SoundPlayer(str);
+            snd.Play();
+        }
+
+
+    }
+}
